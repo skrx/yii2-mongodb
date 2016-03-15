6b93d52a495d8f61f8c71a7408a7873a18c9ca05
Zhao vistart <i@vistart.name>
2016-03-15 20:43:58 +0800

    remove redundant statements.

--- tests/data/travis/mongodb-setup.sh
+++ tests/data/travis/mongodb-setup.sh
@@ -2,14 +2,10 @@
 #
 # install mongodb
 
-pecl install mongodb
 echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 echo "MongoDB Server version:"
 mongod --version
-
-echo "MongoDB PHP Extension version:"
-php -i |grep mongodb -4 |grep -2 Version
 
 # enable text search
 mongo --eval 'db.adminCommand( { setParameter: true, textSearchEnabled : true})'
